3a871410ebc08be8fc2c3b31efda12311c9bfa1b
Enrique Sanchez <enrike.sanchez@gmail.com>
2021-09-01 16:51:27 -0600

    Fixed the Product

--- src/main/java/mx/tec/web/lab/manager/ProductManager.java
+++ src/main/java/mx/tec/web/lab/manager/ProductManager.java
@@ -11,7 +11,6 @@ import java.util.Optional;
 
 import javax.annotation.Resource;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import mx.tec.web.lab.dao.ProductDAO;
